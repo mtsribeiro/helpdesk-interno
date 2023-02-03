e98554740ebc88f934e1c6701630e3489d8b8c35
Mateus Ribeiro <98188179+mtsribeiro@users.noreply.github.com>
2023-02-03 18:54:30 -0300

    .

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 var multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-      cb(null, '/var/www/site_helpdesk/uploads')
+      cb(null, '/var/www/site_helpdesk/helpdesk-interno/uploads')
   },
   filename: function (req, file, cb) {
       // Extração da extensão do arquivo original:
