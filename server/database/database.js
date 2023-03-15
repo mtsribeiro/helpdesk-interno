bd7fa92688a4d0458b925042921d6db42ab99346
Mateus Ribeiro <98188179+mtsribeiro@users.noreply.github.com>
2023-03-15 14:08:15 -0300

    .

--- server/database/database.js
+++ server/database/database.js
@@ -1,14 +1,5 @@
 const mysql = require("promise-mysql");
 
-const connection = mysql.createConnection({
-  host: "assinatura.rstransitarios.com.br",
-  user: "mateus",
-  port: "3306",
-  password: "21uZtxOj8HWhf1T4",
-  database: "helpdesk",
-  charset: "utf8mb4",
-});
-
 async function getConnection() {
   let conn = await mysql.createConnection({
   host: "assinatura.rstransitarios.com.br",
